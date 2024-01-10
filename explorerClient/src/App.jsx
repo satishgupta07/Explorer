d66b7cbbdec5d5f59377956ebda615271a9580ee
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2024-01-11 00:37:59 +0530

    Add the protected route - AuthLayout

--- explorerClient/src/App.jsx
+++ explorerClient/src/App.jsx
@@ -6,6 +6,7 @@ import ProfilePage from "./pages/ProfilePage";
 import Register from "./pages/Register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserProfile from "./pages/UserProfile";
+import AuthLayout from "./components/AuthLayout";
 
 function App() {
   return (
@@ -13,11 +14,35 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route
+            exact
+            path="/"
+            element={
+              <AuthLayout>
+                <Home />
+              </AuthLayout>
+            }
+          />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/register" element={<Register />} />
-          <Route exact path="/profile" element={<ProfilePage />} />
-          <Route exact path="/profile/:userid" element={<UserProfile />} />
+          <Route
+            exact
+            path="/profile"
+            element={
+              <AuthLayout>
+                <ProfilePage />
+              </AuthLayout>
+            }
+          />
+          <Route
+            exact
+            path="/profile/:userid"
+            element={
+              <AuthLayout>
+                <UserProfile />
+              </AuthLayout>
+            }
+          />
           {/* Wildcard route for undefined paths. Shows a 404 error */}
           <Route path="*" element={<p>404 Not found</p>} />
         </Routes>
---
+++ explorerClient/src/components/AuthLayout.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+
+export default function AuthLayout({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+  const { user, token, logout } = useAuth();
+
+  useEffect(() => {
+    let authStatus;
+    if (user && token) {
+      authStatus = true;
+    } else {
+      authStatus = false;
+    }
+
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+      logout();
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [user, token, navigate, authentication]);
+
+  return loader ? <h1>Loading...</h1> : <>{children}</>;
+}
